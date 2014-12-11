27e0551fa0fe52df083f20e04c7c0419d16e6fae
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-11 18:33:51 -0500

    slight reformatting for clarity in User model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe User do
   let(:new_user) { User.new(name: 'waffles', email: 'idk@gmail.com', password: 'password123')}
+
   it 'should save new User instances to the database with valid input' do
     expect(new_user.save).to eq true
     expect(User.all.count).to eq 1
   end
+
 end
