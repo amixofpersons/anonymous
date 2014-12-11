c16fe7b1d1794308c33568292e7e03f41cdcaaa1
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-11 17:40:46 -0500

    add Post model

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ActiveRecord::Base
+  belongs_to :poster, class_name: "User", foreign_key: "user_id"
+
+  has_many :comments
+  has_many :votes, :as => :votable
+
+  validates :poster, presence: true
+
+  def number_of_votes
+    self.votes.size
+  end
+end
