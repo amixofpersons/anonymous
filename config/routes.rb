841d7ee9a7113f11dd12979ab4340ce0d4d0be08
dakotameadows <dakota.w.meadows@gmail.com>
2014-12-11 19:02:09 -0500

    Set up comments and posts controllers/routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  root to: 'homepage'
+
+  resource :posts, only: [:new, :show, :create, :destroy, :index]
+
+  resources :posts do
+    resources :comments
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
