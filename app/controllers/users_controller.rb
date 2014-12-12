253380bdf75c6920f4ab0c7134adaa70b16215ae
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-12 11:11:36 -0500

    add stong params for user signup, user creation works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new params[:user]
+    @user = User.new(user_params)
     if @user.save
-      redirect_to post_path
+      redirect_to posts_path
     else
       render :new
     end
@@ -15,4 +15,9 @@ class UsersController < ApplicationController
 
   def index
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
