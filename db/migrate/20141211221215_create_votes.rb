40a01626cc5b7a42c41a5dbe60e325d2861e30f2
Dmitriy Istomin <dmitriy.istomin@ncf.edu>
2014-12-11 19:17:50 -0500

    fix SUPER FUCKING ANNOYING TYPO in the migration and re-migrate

--- db/migrate/20141211221215_create_votes.rb
+++ db/migrate/20141211221215_create_votes.rb
@@ -2,7 +2,7 @@ class CreateVotes < ActiveRecord::Migration
   def change
     create_table   :votes do |t|
       t.references :user
-      t.references :votable, ploymorphic: true
+      t.references :votable, polymorphic: true
     end
   end
 end
