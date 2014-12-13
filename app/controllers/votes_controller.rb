7a1ec2c4c7949fc9666511e4edf5f2042d4a4d4e
Liam McCartney <liam.mccartney@gmail.com>
2014-12-13 13:49:40 -0500

    use instance variable

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,14 @@
 class VotesController < ApplicationController
 
+  def create
+    user = current_user
+    if params[:post_id]
+      post = Post.find(params[:post_id])
+    else
+      comment = Comment.find(params[:comment_id])
+    end
+    @vote = user.votes.create
+    post.votes << @vote
+    redirect_to post_path(post)
+  end
 end
