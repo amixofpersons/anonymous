661dce8f9308a2f79e62671720bc8e2140a33ed1
Liam McCartney <liam.mccartney@gmail.com>
2014-12-13 13:51:45 -0500

    test vote/post association

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -11,5 +11,11 @@ describe VotesController do
         post_id: test_post.id
       }.to change {Vote.count}.by (1)
     end
+
+    it "should associate a vote with a post" do
+      session[:user_id] = test_user.id
+      post :create, post_id: test_post.id
+      expect(assigns(:vote).votable).to eq(test_post)
+    end
   end
 end
