d71256c38ac1a6f02fad677e38ac561f76cb3bcd
Liam McCartney <liam.mccartney@gmail.com>
2014-12-13 14:03:30 -0500

    conditional vote creation

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,13 +2,15 @@ class VotesController < ApplicationController
 
   def create
     user = current_user
+    @vote = user.votes.create
     if params[:post_id]
       post = Post.find(params[:post_id])
+      post.votes << @vote
+      redirect_to post_path(post)
     else
       comment = Comment.find(params[:comment_id])
+      comment.votes << @vote
+      redirect_to post_path(comment.post)
     end
-    @vote = user.votes.create
-    post.votes << @vote
-    redirect_to post_path(post)
   end
 end
