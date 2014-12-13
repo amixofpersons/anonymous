783622e71cf25fb24b9bd3677731080f9d768fd8
Liam McCartney <liam.mccartney@gmail.com>
2014-12-13 13:52:43 -0500

    test vote/user association

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -17,5 +17,11 @@ describe VotesController do
       post :create, post_id: test_post.id
       expect(assigns(:vote).votable).to eq(test_post)
     end
+
+    it "should associate a vote with current user" do
+      session[:user_id] = test_user.id
+      post :create, post_id: test_post.id
+      expect(assigns(:vote).user).to eq(test_user)
+    end
   end
 end
