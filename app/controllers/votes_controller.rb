46be3a081be1cc8045f3dc7206820cce12a3bdab
Liam McCartney <liam.mccartney@gmail.com>
2014-12-13 15:16:12 -0500

    use already_voted? helper method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,10 +2,10 @@ class VotesController < ApplicationController
 
   def create
     user = current_user
-    @vote = user.votes.create
+    @vote = user.votes.new
     if params[:post_id]
       post = Post.find(params[:post_id])
-      post.votes << @vote
+    post.votes << @vote unless already_voted?(post)
       redirect_to post_path(post)
     else
       comment = Comment.find(params[:comment_id])
