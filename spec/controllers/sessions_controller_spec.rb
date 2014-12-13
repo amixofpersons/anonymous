b3476758edda67204d51a1ebaf650cb0f3bdd5cd
maggie <myustinreyes@gmail.com>
2014-12-13 12:54:47 -0500

    setting up what tables should be

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe SessionsController, :type => :controller do
     expect(response).to be_success
   end
 
-  it "redirects to the posts home page upon save" do
+  it "signs in a user if fields are filled correctly" do
     post :create, user: FactoryGirl.attributes_for(:user)
     expect(response).to redirect_to posts_path
+  end
+
+  it "doesn't sign in a user if fields are filled incorrectly" do
   end
 end
