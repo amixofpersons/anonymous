7659c4c6173be41590ef11d85200a4fbd3aa8565
Liam McCartney <liam.mccartney@gmail.com>
2014-12-13 13:29:45 -0500

    begin create testing

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 describe VotesController do
-
+  let (:test_user) {FactoryGirl.create :user}
+  let (:test_post) {FactoryGirl.create :post}
+  describe "#create" do
+    it "should create a new vote" do
+      session[:user_id] = test_user.id
+      expect{
+        post :create,
+        post_id: test_post.id,
+        user_id: test_user.id
+      }.to change {Vote.count}.by (1)
+    end
+  end
 end
