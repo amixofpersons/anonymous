7491dd9ebdb77aa3f33d1dd15e7e365b7f07b92f
maggie <myustinreyes@gmail.com>
2014-12-13 13:00:14 -0500

    finished sessions spec for the most part

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,15 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, :type => :controller do
-  it "renders the login page" do
+  it "loads the page" do
+    get :new, user: FactoryGirl.attributes_for(:user)
     expect(response).to be_success
-  end
-
-  it "signs in a user if fields are filled correctly" do
-    post :create, user: FactoryGirl.attributes_for(:user)
-    expect(response).to redirect_to posts_path
-  end
-
-  it "doesn't sign in a user if fields are filled incorrectly" do
   end
 end
