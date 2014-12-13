2f212ec29cb768f1e26f9464e23715cce618f971
Liam McCartney <liam.mccartney@gmail.com>
2014-12-13 14:03:52 -0500

    test for voting on a comment

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe VotesController do
   let (:test_user) {FactoryGirl.create :user}
   let (:test_post) {FactoryGirl.create :post}
+  let (:test_comment) {test_user.comments.create(FactoryGirl.attributes_for :comment)}
   describe "#create" do
     it "should create a new vote" do
       session[:user_id] = test_user.id
@@ -16,6 +17,13 @@ describe VotesController do
       session[:user_id] = test_user.id
       post :create, post_id: test_post.id
       expect(assigns(:vote).votable).to eq(test_post)
+    end
+
+    it "should associate a vote with a comment" do
+      session[:user_id] = test_user.id
+      test_post.comments << test_comment
+      post :create, comment_id: test_comment.id
+      expect(assigns(:vote).votable).to eq(test_comment)
     end
 
     it "should associate a vote with current user" do
