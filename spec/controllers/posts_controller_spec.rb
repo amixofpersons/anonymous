c4a52b6a7709bcf31e57fda26987010fdbb22215
Liam McCartney <liam.mccartney@gmail.com>
2014-12-12 22:09:04 -0500

    test post belongs to user

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,10 +4,18 @@ describe PostsController do
   let (:test_post) {FactoryGirl.create :post}
   let (:test_user) {FactoryGirl.create :user}
 
-  it 'should save new Post instances to the database with valid input' do
-    expect {
+  describe "#create" do
+    it 'should save new Post instances to the database with valid input' do
+      expect {
       session[:user_id] = test_user.id
-    post :create, user_id: test_user.id, post: {title: 'my title', body: 'my body text'}
-    }.to change {Post.count}.by(1)
+      post :create, user_id: test_user.id, post: {title: 'my title', body: 'my body text'}
+      }.to change {Post.count}.by(1)
+    end
+
+    it 'should belong to a user' do
+      session[:user_id] = test_user.id
+      post :create, user_id: test_user.id, post: {title: 'my title', body: 'my body text'}
+      expect(assigns(:post).poster).to eq test_user
+    end
   end
 end
