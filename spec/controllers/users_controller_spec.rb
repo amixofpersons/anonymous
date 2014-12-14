7d8f6bf529c7dfdb4829f18b21f96096afbae8ad
Liam McCartney <liam.mccartney@gmail.com>
2014-12-14 16:51:49 -0500

    begin testing showing a user likes

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
-  it "redirects to the posts home page upon save" do
-    get :new, user: FactoryGirl.attributes_for(:user)
-    expect(response).to render_template(:new)
+  let (:test_user) {FactoryGirl.create :user}
+  describe "#new" do
+    it "redirects to the posts home page upon save" do
+      get :new, user: FactoryGirl.attributes_for(:user)
+      expect(response).to render_template(:new)
+    end
   end
 
-  it "redirects to the posts home page upon save" do
-    post :create, user: FactoryGirl.attributes_for(:user)
-    expect(response).to redirect_to posts_path
+  describe "#create" do
+    it "redirects to the posts home page upon save" do
+      post :create, user: FactoryGirl.attributes_for(:user)
+      expect(response).to redirect_to posts_path
+    end
+  end
+
+  describe "#show" do
+    it "shows a user all of their 'likes'" do
+      get :show, id: test_user.id
+      expect(assigns(:votes)).to eq(test_user.votes)
+    end
   end
 end
 
