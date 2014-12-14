eb86abddaf185cd2e02c0f1b643c94872c51458e
Liam McCartney <liam.mccartney@gmail.com>
2014-12-14 15:13:48 -0500

    remove unecessary ajax

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,14 +1,5 @@
 $(document).ready(function(){
   $(".secrets").on('click', "#new-secret-link",function(event){
     event.preventDefault();
-    $target = $(event.target);
-
-    $.ajax({
-      type: 'GET',
-      url: $target.attr('href'),
-      data: $target.serialize()
-    }).done(function(response){
-      $target.parent().html(response)
-    });
   });
 });
