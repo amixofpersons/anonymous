ea51819e3e1f139b76b2ddc230ca151ff57c1092
maggie <myustinreyes@gmail.com>
2014-12-14 13:07:41 -0500

    adding respond_to form for ajax

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,18 +1,36 @@
 class VotesController < ApplicationController
 
   def create
+
     user = current_user
     @vote = user.votes.new
-    if params[:post_id]
-      post = Post.find(params[:post_id])
-      post.votes << @vote unless already_voted?(post)
-      post = Post.find(params[:post_id])
-      post.votes << @vote unless already_voted?(post)
-      redirect_to :back
-    else
-      comment = Comment.find(params[:comment_id])
-      comment.votes << @vote
-      redirect_to post_path(comment.post)
+    respond_to do |format|
+      if params[:post_id]
+        format.html {
+          post = Post.find(params[:post_id])
+          post.votes << @vote unless already_voted?(post)
+          redirect_to :back
+        }
+        format.js {
+          @post = Post.find(params[:post_id])
+          @post.votes << @vote unless already_voted?(@post)
+        }
+      else
+        format.html {
+          comment = Comment.find(params[:comment_id])
+          comment.votes << @vote
+          redirect_to post_path(comment.post)
+        }
+        format.js {
+          comment = Comment.find(params[:comment_id])
+          comment.votes << @vote
+        }
+      end
     end
   end
+
+  private
+    def vote_params
+      params.require(:vote).permit(:post_id, :comment_id)
+    end
 end
