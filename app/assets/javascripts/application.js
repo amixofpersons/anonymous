3294492770a327f346d145f045a40faf630d7aa0
maggie <myustinreyes@gmail.com>
2014-12-14 13:07:08 -0500

    single responsibility jquery implemented

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,30 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require nav
+//= require vote
 //= require_tree .
-$(document).ready(function(){
-  $('.nav-list').hide();
-
-  $('.menu-toggle').mouseover(function(event) {
-    event.preventDefault();
-    $('.nav-list').show();
-  });
-
-  $('.menu-toggle').mouseout(function(event){
-    $('.nav-list').hide();
-  })
-
-  $('.vote').bind('click', function(){
-    event.preventDefault();
-    var $target = $(event.target);
-    console.log($target);
-
-    $.ajax({
-      type: "POST",
-      url: $target.attr("votes_path")
-    })
-    .done(function(response){
-      $(response).html("some text")
-    })
-  })
-})
