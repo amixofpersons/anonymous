48a37b70dbd7ec14ecb68e26f8ae5ca63e110706
Liam McCartney <liam.mccartney@gmail.com>
2014-12-13 20:00:13 -0500

    ajax for new post form

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,14 +1,14 @@
-// $(document).ready(function(){
-//   $(".secrets").on('click', "#new-secret-link",function(event){
-//     // event.preventDefault();
-//     $target = $(event.target);
+$(document).ready(function(){
+  $(".secrets").on('click', "#new-secret-link",function(event){
+    event.preventDefault();
+    $target = $(event.target);
 
-//     $.ajax({
-//       type: 'GET',
-//       url: $target.href,
-//       data: $target.serialize()
-//     }).done(function(response){
-//       $target.html(response);
-//     });
-//   });
-// });
+    $.ajax({
+      type: 'GET',
+      url: $target.attr('href'),
+      data: $target.serialize()
+    }).done(function(response){
+      $target.parent().html(response)
+    });
+  });
+});
