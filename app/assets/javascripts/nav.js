da8cdbc69aeb5c32a42f0d76fec69ce5d214b9fb
maggie <myustinreyes@gmail.com>
2014-12-14 15:15:16 -0500

    jquery not responding correctly

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
   $('.menu-toggle').on('mouseenter',function() {
-    console.log("I'm still hovering!")
     $('.nav-list').css('display', 'inline')
   });
 
   $('.menu-toggle').bind('mouseleave', function(){
-    console.log("I've left this menu!")
     $('.nav-list').css('display', 'inline')
     $('.nav-list').hide();
   });
