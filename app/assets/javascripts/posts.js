80a6027556f7ecec635777107c43cde49d1d2095
Liam McCartney <liam.mccartney@gmail.com>
2014-12-14 15:43:48 -0500

    event handler for new post

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
   $(".secrets").on('click', "#new-secret-link",function(event){
     event.preventDefault();
   });
+
+  $(".homepage").on('submit', function(event){
+    event.preventDefault();
+  })
 });
