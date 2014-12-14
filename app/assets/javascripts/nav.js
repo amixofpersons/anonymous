e1f4eb20d3bde307e91c993ab99d22efc206a73b
maggie <myustinreyes@gmail.com>
2014-12-14 14:49:35 -0500

    fixed original error, have new one

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
   $('.menu-toggle').on('mouseenter',function() {
     $('.nav-list').css('display', 'inline')
   });
-
   $('.menu-toggle').bind('mouseleave', function(){
     $('.nav-list').css('display', 'inline')
     $('.nav-list').hide();
