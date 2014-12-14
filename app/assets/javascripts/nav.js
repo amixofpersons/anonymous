cc0f61bfdb937ea5db64ed4dd2d236c018318801
Liam McCartney <liam.mccartney@gmail.com>
2014-12-14 15:43:26 -0500

    nothing important

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.nav-list').hide();
 
-  $('.menu-toggle').mouseover(function(event) {
+  $('.menu-toggle').mouseenter(function(event){
     event.preventDefault();
     $('.nav-list').show();
   });
